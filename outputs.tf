305e9b0213993ed5dccb51798c79330bf80bf1ae
Alexander Grigoriev <an.grigoriev84@gmail.com>
2023-01-23 22:23:45 +0300

    modify outputs

--- outputs.tf
+++ outputs.tf
@@ -1,26 +1,31 @@
-output "network_id" {
-  description = "The ID of the network"
-  value       = yandex_vpc_network.this.id
-}
-
-output "subnet_ids" {
-  description = "IDs of all created subnets"
-  value       = [for subnet in yandex_vpc_subnet.this : subnet.id]
-}
-
-output "availability_zones" {
-  description = "Availability zones of all created subnets"
-  value       = [for subnet in yandex_vpc_subnet.this : subnet.zone]
+output "network" {
+  description = "Network info"
+  value       = yandex_vpc_network.this
 }
 
 output "subnets" {
-  description = "Map of subnets with additional info"
-  value = { for v in yandex_vpc_subnet.this : v.name => {
-    "id"          = v.id,
-    "name"        = v.name,
-    "zone"        = v.zone,
-    "cidr"        = v.v4_cidr_blocks,
-    "nat_enabled" = v.route_table_id == yandex_vpc_route_table.nat.id ? true : false
-    }
+  description = "Subnets info"
+  value = ({
+    for subnet in yandex_vpc_subnet.this :
+    subnet.name => merge(
+      subnet, {
+        "nat_enabled" = (
+          subnet.route_table_id == yandex_vpc_route_table.nat.id
+          ? true
+          : false
+        )
+      }
+    )
+  })
+}
+
+output "rt" {
+  value = {
+    "rt_main" = yandex_vpc_route_table.this,
+    "rt_nat"  = yandex_vpc_route_table.nat
   }
+}
+
+output "gateway" {
+  value = yandex_vpc_gateway.nat
 }
